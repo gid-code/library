c64ef0bd1d9cdffe325cfd48b554b370dc0fa31b
gid-code <gideonpeprah94@gmail.com>
2019-12-19 11:16:31 +0000

    added new routes

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 
 Route::post('/books', 'BooksController@store');
 Route::patch('/books/{book}', 'BooksController@update');
+Route::delete('/books/{book}', 'BooksController@destroy');
+
+Route::post('/author', 'AuthorsController@store');
